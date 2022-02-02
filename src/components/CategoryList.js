9153a3fb961ab24f9dd3b3ec30a86eb411e9c1c7
Taeho Kim <taeho@taeho.io>
2022-02-03 00:56:50 +0900

    Add firebase analytics

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {FlatList} from 'react-native';
+import {useTailwind} from 'tailwind-rn';
+
 import CategoryListItem from './CategoryListItem';
 
 function CategoryList({categories}) {
+  const tailwind = useTailwind();
+
   return (
     <FlatList
       data={categories}
